64aa494978f78c71e62ef343e7caca173fff00d3
xiyifenheart <543597191@qq.com>
2020-12-28 10:30:16 +0800

    feat: refactor transfer 30%

--- packages/element3/packages/transfer/src1/TransferPanel.vue
+++ packages/element3/packages/transfer/src1/TransferPanel.vue
@@ -1,59 +1,61 @@
 <template>
-    <div class="el-transfer-panel">
-        <el-checkbox-group v-model="checked" @change="checkedChangeHandler">
-            <el-checkbox
-                class="el-transfer-panel__item"
-                :label="item.key"
-                :key="item.key"
-                v-for="item in data"
-            >
-            </el-checkbox>
-        </el-checkbox-group>
-    </div>
+  <div class="el-transfer-panel">
+    <el-checkbox-group v-model="checked" @change="checkedChangeHandler">
+      <el-checkbox
+        class="el-transfer-panel__item"
+        :label="item.key"
+        :key="item.key"
+        v-for="item in data"
+      >
+      </el-checkbox>
+    </el-checkbox-group>
+  </div>
 </template>
 <script>
 import ElCheckboxGroup from '../../checkbox-group'
 import ElCheckbox from '../../checkbox'
-import { ref } from 'vue'
+import { reactive, toRefs, computed } from 'vue'
 export default {
-    name: 'ElTransferPanel',
-    emits: ['checked-change'],
-    components: {
-        ElCheckboxGroup,
-        ElCheckbox
+  name: 'ElTransferPanel',
+  emits: ['checked-change'],
+  components: {
+    ElCheckboxGroup,
+    ElCheckbox
+  },
+  props: {
+    data: {
+      type: Array,
+      default() {
+        return []
+      }
     },
-    props: {
-        data: {
-            type: Array,
-            default() {
-                return []
-            }
-        },
-        defaultChecked: {
-            type: Array,
-            default() {
-                return []
-            }
-        }
-    },
-    setup(props, { emit }) {
-        const { defaultChecked } = props
-        let checked = ref([])
-        
-        if (defaultChecked.length > 0) {
-            checked.value = defaultChecked
-            emit('checked-change', defaultChecked)
-        }
-        
-
-        const checkedChangeHandler = (val) => {
-            emit('checked-change', val)
-        }
-
-        return {
-            checked,
-            checkedChangeHandler
-        }
+    defaultChecked: {
+      type: Array,
+      default() {
+        return []
+      }
     }
+  },
+  setup(props, { emit }) {
+    let state = reactive({
+      checked: []
+    })
+
+    if (props.defaultChecked.length > 0) {
+      state.checked = computed(() => {
+        return props.defaultChecked
+      })
+      emit('checked-change', state.checked)
+    }
+
+    const checkedChangeHandler = (val) => {
+      emit('checked-change', val)
+    }
+
+    return {
+      ...toRefs(state),
+      checkedChangeHandler
+    }
+  }
 }
 </script>
